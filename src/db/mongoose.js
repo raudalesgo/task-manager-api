98a862789f3e83ee2eb9e9e51fd025b6564d1bd4
Gerlad Hurtado <raudalesgo@gmail.com>
2022-11-07 14:42:53 -0600

    add mongoose config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useUnifiedTopology: true,
     useFindAndModify: false
 })
-//mongodb://127.0.0.1:27017/task-manager-api
