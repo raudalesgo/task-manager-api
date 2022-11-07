99fe1ca9f116b59d06a1b4ee0bddd9a2d5649a51
Gerlad Hurtado <raudalesgo@gmail.com>
2022-11-07 14:58:33 -0600

    re formart

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,14 +4,14 @@ const router = new express.Router();
 const auth = require("../middleware/auth");
 const multer = require("multer");
 const sharp = require("sharp");
-const {sendWelcomeEmail, sendCancelationEmail} = require("../emails/account")
+const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    sendWelcomeEmail(user.email,user.name);
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
@@ -84,7 +84,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
-    sendCancelationEmail(req.user.email,req.user.name);
+    sendCancelationEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch (e) {
     res.status(500).send();
@@ -103,8 +103,15 @@ const upload = multer({
   },
 });
 
-router.post("/users/me/avatar", auth, upload.single("avatar"), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
     req.user.avatar = buffer;
     await req.user.save();
     res.send();
