566a5a3fb5739009e8ad39a5991331f3f1821b9d
Gerlad Hurtado <raudalesgo@gmail.com>
2022-11-07 14:26:53 -0600

    add version node and  npm, add .npmrc

--- src/index.js
+++ src/index.js
@@ -6,33 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT
 
-
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000,
-    },
-        fileFilter(req,file, cb){
-            if(!file.originalname.match(/\.(doc|docx)$/)){
-                return cb(new Error('please upload a word document'));
-            }
-
-            // callback(new Error('File must be a PDF file'));
-            cb(undefined, true);
-            // callback(undefined, false);
-
-        }
-    
-})
-
-app.post('/upload', upload.single('upload'),(req,res) =>{
-    res.send()
-},(error, req, res, next) =>{
-    res.status(400).send({error: error.message});
-})
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
